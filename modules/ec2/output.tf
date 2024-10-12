ee1e5fb11abfe05cad6fe25d683de403a40726b4
youssef helmy <96450513+yossef122@users.noreply.github.com>
2024-10-12 23:17:45 +0300

    edit the id to ip
    Signed-off-by: youssef helmy <96450513+yossef122@users.noreply.github.com>

--- modules/ec2/output.tf
+++ modules/ec2/output.tf
@@ -1,7 +1,7 @@
-output "ec2-id" {
-  value = aws_instance.project.id
+output "ec2-ip" {
+  value = aws_instance.project.ip
 }
 
 output "ec2-ssh" {
-  value = "ubuntu@${aws_instance.project.id}"
+  value = "ubuntu@${aws_instance.project.ip}"
 }
